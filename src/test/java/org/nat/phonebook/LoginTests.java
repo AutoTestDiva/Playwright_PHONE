1ac88b700b7e617ce19276c4001d67a630057ce7
AutoTestDiva <nataliia.autoqa@gmail.com>
2025-03-10 21:42:51 +0100

    LoginTests-1

--- src/test/java/org/nat/phonebook/LoginTests.java
+++ src/test/java/org/nat/phonebook/LoginTests.java
@@ -5,8 +5,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CreateAccountTests extends TestBase {
-
+public class LoginTests extends TestBase {
     @BeforeMethod
     public void ensurePrecondition() {
         if (!isElementPresent(By.cssSelector("a:nth-child(4)"))) {
@@ -16,19 +15,10 @@ public class CreateAccountTests extends TestBase {
     }
 
     @Test
-    public void newUserRegistrationPositiveTest() {
+    public void loginPositiveTest(){
         type(By.cssSelector("[placeholder ='Email']"), "2025@gmail.com");
         type(By.cssSelector("[placeholder ='Password']"), "Test2025!");
-        // driver.findElement(By.xpath("//button[text()='Registration']")).click();
         click(By.cssSelector("button[name='login']"));
         Assert.assertTrue(isElementPresent2(By.xpath("//button[text()='Sign Out']")));
-    }
-
-    @Test
-    public void existedUserRegistrationNegativeTest() {
-        type(By.cssSelector("[placeholder ='Email']"), "2025@gmail.com");
-        type(By.cssSelector("[placeholder ='Password']"), "Test2025!");
-        click(By.xpath("//button[text()='Registration']"));
-        Assert.assertTrue(isAlertPresent());
     }
 }
--- src/test/java/org/nat/phonebook/TestBase.java
+++ src/test/java/org/nat/phonebook/TestBase.java
@@ -1,8 +1,11 @@
 package org.nat.phonebook;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -42,5 +45,27 @@ public class TestBase {
     @AfterMethod (enabled=false)
     public void tearDown() {
         driver.quit();
+    }
+
+    public void click(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        driver.findElement(locator).clear();
+        driver.findElement(locator).sendKeys(text);
+    }
+
+    public boolean isAlertPresent() {
+        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.alertIsPresent());
+        if (alert == null) {
+            return false;
+        } else {
+            driver.switchTo().alert();
+            alert.accept();
+            return true;
+        }
     }
 }
