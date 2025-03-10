12bf5e14cee0f599f37277d38cad82b514519672
AutoTestDiva <nataliia.autoqa@gmail.com>
2025-03-10 22:18:29 +0100

    AddContactTests-1

---
+++ src/test/java/org/nat/phonebook/AddContactTests.java
@@ -0,0 +1,30 @@
+package org.nat.phonebook;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AddContactTests extends TestBase{
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (!isElementPresent(By.cssSelector("a:nth-child(4)"))) {
+            click(By.xpath("//button[contains(.,'Sign Out']"));
+        }
+        click(By.cssSelector("a:nth-child(4)"));
+        type(By.cssSelector("[placeholder ='Email']"), "2025@gmail.com");
+        type(By.cssSelector("[placeholder ='Password']"), "Test2025!");
+        click(By.cssSelector("button[name='login']"));
+        click(By.cssSelector("a[href='/add']"));
+    }
+    @Test
+    public void addContactPositiveTest(){
+    click(By.cssSelector("input[placeholder='Name']"));
+        type(By.cssSelector("input[placeholder='Name']"), "Max");
+        type(By.cssSelector("input[placeholder='Last Name']"), "Mayer");
+        type(By.cssSelector("input[placeholder='Phone']"), "49123456789");
+        type(By.cssSelector("input[placeholder='email']"), "Max@gmail.com");
+        type(By.cssSelector("input[placeholder='Address']"), "Leipzig");
+        type(By.cssSelector("input[placeholder='description']"), "Software tester");
+        click(By.cssSelector("div[class='add_form__2rsm2'] button"));
+    }
+}
