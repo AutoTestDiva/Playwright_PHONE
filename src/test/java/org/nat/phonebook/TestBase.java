64551a2482d73dcf1e10506ad0d2af6806692b58
AutoTestDiva <nataliia.autoqa@gmail.com>
2025-03-15 22:27:38 +0100

    logger added

--- src/test/java/org/nat/phonebook/TestBase.java
+++ src/test/java/org/nat/phonebook/TestBase.java
@@ -1,22 +1,36 @@
 package org.nat.phonebook;
 
 import org.nat.phonebook.framework.ApplicationManager;
-import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", "chrome"));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() {
         app.init();
     }
 
-    @AfterMethod(enabled=false)
+    @AfterSuite
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void startTest(){
+        logger.info("Start nest");
+    }
+
+    @AfterMethod
+    public void stopTest(){
+        logger.info("Stop test");
     }
 
 }
